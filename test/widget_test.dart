aedc7788e7ef00355237cb8d42ee435d381f90dc
Helio S. Junior <helio.junior@codeminer42.com>
2018-11-01 22:56:26 -0300

    Revert "Added to readme the badge, fixed travis.yml and refactored the test"
    This reverts commit e3091fe7144472d4966c753fd858e1b8b392fe94.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,11 +10,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:weight_tracker/main.dart';
 
 void main() {
-  testWidgets('Appbar contains app name', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('Tracker'), findsOneWidget);
+    expect(find.text('Weight Tracker'), findsOneWidget);
   });
 }
